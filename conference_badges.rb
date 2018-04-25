c8bf27f63184c30b59254ffee8af50d889727a18
Eric Chrobak <eric@ericchrobak.com>
2018-04-25 01:01:47 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,15 +1,20 @@
 # Write your code here.
 
 def badge_maker(name)
-  return "Hello, my name is #{name}"
+  return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array)
-  new_array = []
+  badge_messages = []
   counter = 0
   array.each |e|
-    new_array[counter] = badge_maker(e)
+    badge_messages[counter] = badge_maker(e)
     counter += 1
   end
-  return new_array
+  return badge_messages
+end
+
+
+def assign_rooms(array)
+  new_array
 end
