26a4022ce4ca1ac604b20b37284b9ae01fb76315
Eric Chrobak <eric@ericchrobak.com>
2018-04-25 00:57:13 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,5 +5,11 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-
+  new_array = []
+  counter = 0
+  array.each |e|
+    array[counter] = badge_maker(e)
+    counter += 1
+  end
+  return new_array
 end
